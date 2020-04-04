42213509611e27f2d38b0584268033400f85afb6
devill <devillmeister@gmail.com>
2020-04-04 10:52:36 +0200

    smart all in

--- player.rb
+++ player.rb
@@ -6,21 +6,32 @@ class Player
   VERSION = "Getting a little smarter"
 
   def bet_request(game_state)
+    safe_bet(game_state)
+  rescue
+    0
+  end
+
+  def showdown(game_state)
+
+  end
+
+  private
+
+  def safe_bet(game_state)
     me = game_state['players'][game_state['in_action']]
     my_cards = me['hole_cards']
     if my_cards[0]['rank'] == my_cards[1]['rank']
       10000
     else
-      if SecureRandom.random_number <0.3
+      if SecureRandom.random_number <0.1
         10000
       else
         0
       end
     end
-
   end
 
-  def showdown(game_state)
+  def chen_score(game_state)
 
   end
 end

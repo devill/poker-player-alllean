daa39275271cee11fb5368ed13b71598d29f5f57
devill <devillmeister@gmail.com>
2020-04-04 10:44:37 +0200

    smart all in

--- player.rb
+++ player.rb
@@ -1,10 +1,23 @@
+require 'securerandom'
+
 
 class Player
 
-  VERSION = "Default Ruby folding player"
+  VERSION = "Getting a little smarter"
 
   def bet_request(game_state)
-    10000
+    me = game_state['players'][game_state['in_action']]
+    my_cards = me['hole_cards']
+    if my_cards[0]['rank'] == my_cards[1]['rank']
+      10000
+    else
+      if SecureRandom.random_number <0.3
+        10000
+      else
+        0
+      end
+    end
+
   end
 
   def showdown(game_state)
